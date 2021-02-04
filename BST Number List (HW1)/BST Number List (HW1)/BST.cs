01d3ee3c0c32e7b0e71f28f7cc2eda26acc6fa86
Minh <minh.nt.2108@gmail.com>
2021-02-04 08:26:44 +0700

    Added function to read and store user input in an array

---
+++ BST Number List (HW1)/BST Number List (HW1)/BST.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Number_List__HW1_
+{
+    class Class1
+    {
+    }
+}
---
+++ BST Number List (HW1)/BST Number List (HW1)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace BST_Number_List__HW1_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        
+        static void Main()
+        {
+            Console.WriteLine("Enter a collection of numbers in the range [0, 100], separated by spaces: ");
+            string userInput = Console.ReadLine();
+            string[] numberCollection = userInput.Split(' ');
+            foreach (string number in numberCollection)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("Program finished. Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
