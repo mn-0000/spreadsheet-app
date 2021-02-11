529447cbca984a54bfad2a25a1bee63f2f9e51ba
Minh <minh.nt.2108@gmail.com>
2021-02-12 04:49:32 +0700

    Sets up program form, creates list of random integers, and prints the list to test code

---
+++ HW2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            List<int> randomNumbers = new List<int>(10000);
+            Enumerable.Range(0, 20000);
+            int number;
+            for (int i = 0; i < 10000; i++)
+            {
+                number = rand.Next(0, 20000);
+                randomNumbers.Add(number);
+            }
+            textBox1.Text = string.Join(" ", randomNumbers);
+        }
+    }
+}
