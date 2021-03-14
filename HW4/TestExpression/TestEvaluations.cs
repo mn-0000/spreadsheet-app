876a77872c6d38eb37b6a738db7baecb2d93bf5d
Minh <minh.nt.2108@gmail.com>
2021-03-14 13:26:02 +0700

    Added missing tests for multiply and divide operators due to a misinterpretation of the assignment requirements

--- HW4/TestExpression/TestEvaluations.cs
+++ HW4/TestExpression/TestEvaluations.cs
@@ -22,11 +22,11 @@ namespace CptS321.Tests
             ExpressionTree exp3 = new ExpressionTree("hi+10");
             ExpressionTree exp4 = new ExpressionTree("hello+world");
             ExpressionTree exp5 = new ExpressionTree("B3+nice+3");
-            Assert.AreEqual(exp1, 18.0);
-            Assert.AreEqual(exp2, 40.0);
-            Assert.AreEqual(exp3, 10.0);
-            Assert.AreEqual(exp4, 0.0);
-            Assert.AreEqual(exp5, 3.0);
+            Assert.AreEqual(18.0, exp1.Evaluate());
+            Assert.AreEqual(40.0, exp2.Evaluate());
+            Assert.AreEqual(10.0, exp3.Evaluate());
+            Assert.AreEqual(0.0, exp4.Evaluate());
+            Assert.AreEqual(3.0, exp5.Evaluate());
         }
 
         /// <summary>
@@ -41,27 +41,61 @@ namespace CptS321.Tests
             ExpressionTree exp8 = new ExpressionTree("16-17");
             ExpressionTree exp9 = new ExpressionTree("hello-world");
             ExpressionTree exp10 = new ExpressionTree("A1-bye-2");
-            Assert.AreEqual(exp6, 5.0);
-            Assert.AreEqual(exp7, 0.0);
-            Assert.AreEqual(exp8, -1.0);
-            Assert.AreEqual(exp9, 0.0);
-            Assert.AreEqual(exp10, -2.0);
+            Assert.AreEqual(5.0, exp6.Evaluate());
+            Assert.AreEqual(0.0, exp7.Evaluate());
+            Assert.AreEqual(-1.0, exp8.Evaluate());
+            Assert.AreEqual(0.0, exp9.Evaluate());
+            Assert.AreEqual(-2.0, exp10.Evaluate());
+        }
+
+        [Test]
+        public void TestEvaluateMultiply()
+        {
+            ExpressionTree exp11 = new ExpressionTree("7*8");
+            ExpressionTree exp12 = new ExpressionTree("3*4*5*2");
+            ExpressionTree exp13 = new ExpressionTree("3*0");
+            ExpressionTree exp14 = new ExpressionTree("one*two");
+            ExpressionTree exp15 = new ExpressionTree("A3*four*5");
+            Assert.AreEqual(56.0, exp11.Evaluate());
+            Assert.AreEqual(120.0, exp12.Evaluate());
+            Assert.AreEqual(0.0, exp13.Evaluate());
+            Assert.AreEqual(0.0, exp14.Evaluate());
+            Assert.AreEqual(0.0, exp15.Evaluate());
+        }
+
+        [Test]
+        public void TestEvaluateDivide()
+        {
+            ExpressionTree exp16 = new ExpressionTree("26/4");
+            ExpressionTree exp17 = new ExpressionTree("5/8");
+            ExpressionTree exp18 = new ExpressionTree("0/99");
+            ExpressionTree exp19 = new ExpressionTree("44/0");
+            ExpressionTree exp20 = new ExpressionTree("x/y");
+            ExpressionTree exp21 = new ExpressionTree("z/4");
+            Assert.AreEqual(4.5, exp16.Evaluate());
+            Assert.AreEqual(0.625, exp17.Evaluate());
+            Assert.AreEqual(0.0, exp18.Evaluate());
+            Assert.AreEqual(double.PositiveInfinity, exp19.Evaluate());
+            Assert.AreEqual(double.PositiveInfinity, exp20.Evaluate());
+            Assert.AreEqual(0.0, exp21.Evaluate());
         }
 
         /// <summary>
         /// Test method for the ExpressionTree class, which detects whether the code would throw
         /// a NotSupportedException when the user includes an operator that's not supported.
         /// </summary>
+        [Test]
         public void TestUnsupportedOperator()
         {
-            ExpressionTree exp11 = new ExpressionTree("10%3");
-            Assert.That(() => exp11, Throws.TypeOf<System.NotSupportedException>());
+            ExpressionTree exp0 = new ExpressionTree("10%3");
+            Assert.That(() => exp0, Throws.TypeOf<System.NotSupportedException>());
         }
 
         /// <summary>
         /// Test method for the ExpressionTree class, which determines if the code will evaluate
         /// to infinity if the result goes over the double data type's MaxValue.
         /// </summary>
+        [Test]
         public void TestInfinity()
         {
             var halfMaxValue = (double.MaxValue / 2.0).ToString("F", CultureInfo.InvariantCulture);
